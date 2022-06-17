b222a80072967b1c952254ec504e9ab61c026c74
Roberto Martinez <robertomiguel2001@gmail.com>
2022-06-17 11:29:54 -0700

    implemented infinite loading feature

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 const BASE_IMG_URL = "https://image.tmdb.org/t/p/w500/"
 const API_KEY = "1b5be75491b536d82aea66fdb66143df"
-const PAGES = 1
+let pages = 1
 
 const moviesGridEl = document.getElementById("movies-grid")
 const searchInputEl = document.getElementById("search-input")
-const loadMoreBtnEl = document. getElementById("load-more-movies-btn-container")
+const loaderEl = document.querySelector(".loader")
+
+const showLoading = () => {
+    console.log("IM LOADING")
+    loaderEl.classList.add('show')
+}
+
+const removeLoading = () => {
+    loaderEl.classList.remove('show')
+}
 
 const renderMovies = movies => {
     movies.forEach(movie => {
@@ -12,10 +21,16 @@ const renderMovies = movies => {
         <div class="movie-card">
             <img src="${BASE_IMG_URL + movie.poster_path}" class="movie-poster" alt="${movie.original_title} Poster">
             <div class="movie-title">${movie.title}</div>
-            <div class="movie-votes">${movie.vote_count}</div>
+            <div id="votes-container">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M14 10h4.764a2 2 0 011.789 2.894l-3.5 7A2 2 0 0115.263 21h-4.017c-.163 0-.326-.02-.485-.06L7 20m7-10V5a2 2 0 00-2-2h-.095c-.5 0-.905.405-.905.905 0 .714-.211 1.412-.608 2.006L7 11v9m7-10h-2M7 20H5a2 2 0 01-2-2v-6a2 2 0 012-2h2.5" />
+                </svg>
+                <div id="movie-votes">${movie.vote_count}</div>
+            </div>
         </div>
         `
     })
+    removeLoading()
 }
 
 const render404 = () => {
@@ -27,6 +42,7 @@ const render404 = () => {
 }
 
 searchInputEl.addEventListener("keyup", async e => {
+    moviesGridEl.innerHTML = ""
     const searchInputVal = e.target.value
     if (searchInputVal === "") {
         fetchMovies()
@@ -35,22 +51,31 @@ searchInputEl.addEventListener("keyup", async e => {
     }
 })
 
+window.addEventListener("scroll", () => {
+    const { scrollTop, scrollHeight, clientHeight } = document.documentElement
+
+    if (clientHeight + scrollTop >= scrollHeight - 5) {
+        showLoading()
+        pages += 1
+        setTimeout(fetchMovies, 500)
+    }
+})
+
 const fetchMovies = async () => {
-    moviesGridEl.innerHTML = ""
-    const response = await fetch("https://api.themoviedb.org/3/movie/now_playing?api_key=1b5be75491b536d82aea66fdb66143df&page=1")
+    const response = await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=1b5be75491b536d82aea66fdb66143df&page=${pages}`)
+    if (!response.ok) render404()
+
     const data = await response.json()
     renderMovies(data.results)
 }
 
-const loadMore = () => {
 
-}
 
 const fetchFilteredMovies = async searchInputVal => {
-    moviesGridEl.innerHTML = ""
-    const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${searchInputVal}&page=${PAGES}&include_adult=false`)
+    const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${searchInputVal}&page=${pages}&include_adult=false`)
+    if (!response.ok) render404()
     const filteredData = await response.json()
-    console.log(filteredData.results.length)
+
     if (filteredData.results.length <= 0) render404()
     renderMovies(filteredData.results)
 }
